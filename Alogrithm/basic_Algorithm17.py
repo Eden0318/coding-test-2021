7450d3f2d16bbb9ce128484f7b9fea30aeb94fe9
Eden <haerim2429@gmail.com>
2021-07-28 15:38:37 +0900

    basic_Algorithm17.py

--- Alogrithm/basic_Algorithm17.py
+++ Alogrithm/basic_Algorithm17.py
@@ -1,8 +1,25 @@
-n=9
-a=[list(map(int,input().split())) for _ in range(n)]
+def check(a):
+    for i in range(9):
+        ch1=[0]*10
+        ch2=[0]*10
+        for j in range(9):
+            ch1[a[i][j]]=1
+            ch2[a[j][i]]=1
+        if sum(ch1)!=9 or sum(ch2)!=9:
+            return False
 
-for i in range(n):
-    if set(a[i])<n:
-        print("No")
-    for j in range(n):
-        a[i][j]
+    for i in range(3):
+        for j in range(3):
+            ch3=[0]*10
+            for k in range(3):
+                for s in range(3):
+                    ch3[a[i*3+k][j*3+s]]=1
+            if sum(ch3)!=9:
+                return False
+    return True
+
+a=[list(map(int,input().split())) for _ in range(9)]
+if check(a):
+    print("YES")
+else:
+    print("NO")
--- Alogrithm/test.py
+++ Alogrithm/test.py
@@ -1,6 +1,8 @@
-for m in range(30):
-    for k in range(20):        
-        for i in range(9):
-            print(i)
-            for j in range(10):
-                print(j)
+n=int(input())
+a=list(map(int,input().split()))
+
+for i in range(n):
+    a=map(int,input())
+    for j in range(a//2):
+
+    
