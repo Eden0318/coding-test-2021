33f45ed57462d6acf5c99e77f9d6b68d1450e4f7
Eden <haerim2429@gmail.com>
2021-08-07 14:17:31 +0900

    test.py

--- Alogrithm/test.py
+++ Alogrithm/test.py
@@ -1,8 +1,31 @@
-n=int(input())
-a=list(map(int,input().split()))
+#3
+n,m = map(int,input().split())
+Music = list(map(int,input().split()))
+lt=1
+rt=max(Music)
 
-for i in range(n):
-    a=map(int,input())
-    for j in range(a//2):
+def Count(capacity):
+    cnt=1
+    sum=0
+    for i in  Music:
+        if sum+i>capacity:
+            cnt+=1
+            sum=i
+        else:
+            sum+=i
+    return cnt
 
-    
+
+while lt<=rt:
+    mid=(lt+rt)//2
+    if mid>=maxx and Count(mid)<=m:
+        res=mid
+        rt=mid-1
+    else:
+        lt=mid+1
+
+print(res)
+
+#4
+
+
