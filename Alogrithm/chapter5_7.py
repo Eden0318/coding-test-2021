a792f96969b7470fee8fad6b632ec9f56c7c646b
Eden <haerim2429@gmail.com>
2021-08-23 15:05:39 +0900

    chapter5_7

---
+++ Alogrithm/chapter5_7.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+need=input()
+n=int(input()) #플랜갯수
+for i in range(n):
+    plan=input() #플랜
+    dq=deque(need) #cba
+    for x in plan:
+        if x in dq:
+            if x != dq.popleft():
+                print("#%d NO" (i+1))
+                break
+    else:
+        if len(dq)==0:
+            print("#%d YES" %(i+1))
+        else:
+            print("#%d NO" %(i+1))
+
+
+
