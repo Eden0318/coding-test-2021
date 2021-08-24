f85e5adfcf4a8a248a4c02999616689fa96b4ea4
Eden <haerim2429@gmail.com>
2021-08-24 20:11:20 +0900

    chapter5_5

--- Alogrithm/chapter5_5.py
+++ Alogrithm/chapter5_5.py
@@ -1,14 +1,17 @@
 from collections import deque
-n,k=map(int,input().split())
+n,k=map(int,input().split()) 
 
-dq=list(range(1,n+1)) #1,2,3,4,5,6,7,8
-dq=deque(dq)
+dq=list(range(1,n+1)) 
+dq=deque(dq) 
 
 while dq:
-    for _ in range(len(k-1)):
+    for _ in range(k-1): 
         cur=dq.popleft()
         dq.append(cur)
     dq.popleft()
+    if len(dq)==1:
+        print(dq[0])
+        dq.popleft()
 
         
 
