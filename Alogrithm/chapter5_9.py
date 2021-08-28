ce612dd6d0222553dd21d03c8764c2aeb117d81a
Eden <haerim2429@gmail.com>
2021-08-28 19:53:03 +0900

    chapter5_9

--- Alogrithm/chapter5_9.py
+++ Alogrithm/chapter5_9.py
@@ -1,21 +1,27 @@
-a=input() #AbaAeCe
-b=input() #baeeACA
+a=input() #AbaAeCe 2 1 1 2 1  
+b=input() #baeeACA 1 1 2 2 1  
 str1=dict()
 str2=dict()
-print(str1)
 
-#for x in a:
-#    str1[x]=str1.get(x,0)+1
-#for x in b:
-#    str2[x]=str2.get(x,0)+1
-#
-#for i in str1.keys():
-#    if i in str2.keys():
-#        if str1[i]!=str2[i]:
-#            print("NO")
-#            break
-#    else:
-#        print("NO")
-#        break
-#else:
-#    print("YES")    
+#print(str1)
+
+for x in a:
+    str1[x]=str1.get(x,0)+1
+
+for x in b:
+    str2[x]=str2.get(x,0)+1
+
+#print(str1)
+#print(str2)
+
+
+for i in str1.keys():
+    if i in str2.keys():
+        if str1[i]!=str2[i]:
+            print("NO")
+            break
+    else:
+        print("NO")
+        break
+else:
+    print("YES")    
