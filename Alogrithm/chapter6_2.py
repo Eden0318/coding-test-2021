55de48b6121e043ac8178bbadde08fb4ce26f379
Eden <haerim2429@gmail.com>
2021-09-06 21:27:36 +0900

    chapter6_2

---
+++ Alogrithm/chapter6_2.py
@@ -0,0 +1,31 @@
+#v는 노드값, 전위순회
+def DFS(v):
+    if v>7:
+        return #종료
+    else:
+        print(v,end='')
+        DFS(v*2, end="") #부모곱하기 2 = 왼쪽노드
+        DFS(v*2+1) #오른쪽노드
+
+#v는 노드값, 중위순회
+def DFS(v):
+    if v>7:
+        return #종료
+    else:
+        DFS(v*2, end="") #부모곱하기 2 = 왼쪽노드
+        print(v,end='')
+        DFS(v*2+1) #오른쪽노드
+
+
+#v는 노드값, 후위순회
+def DFS(v):
+    if v>7:
+        return #종료
+    else:
+        DFS(v*2, end="") #부모곱하기 2 = 왼쪽노드
+        print(v,end='')
+        DFS(v*2+1) #오른쪽노드
+
+
+if __name__=="__main__":
+    DFS(1)
