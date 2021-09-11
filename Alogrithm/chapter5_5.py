fc90f0f290611fd3f7404e14fa85626c45bc7230
Eden <haerim2429@gmail.com>
2021-09-11 21:10:29 +0900

    practice7

--- Alogrithm/chapter5_5.py
+++ Alogrithm/chapter5_5.py
@@ -5,10 +5,10 @@ dq=list(range(1,n+1))
 dq=deque(dq) 
 
 while dq:
-    for _ in range(k-1): 
-        cur=dq.popleft()
-        dq.append(cur)
-    dq.popleft()
+    for _ in range(k-1):  #0,1 두번 반복
+        cur=dq.popleft() #!
+        dq.append(cur) #1
+    dq.popleft() #2
     if len(dq)==1:
         print(dq[0])
         dq.popleft()
---
+++ Alogrithm/test6.py
@@ -0,0 +1,7 @@
+str = "Hi my name is eden" 
+splitted_str = str.split() 
+print(splitted_str) 
+
+str2="hi, my name is eden"
+splitted_str = str2.split(",")
+print(splitted_str)
