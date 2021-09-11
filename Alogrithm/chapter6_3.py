548e635a77079844e4788bbbba3e49283fadcfb4
Eden <haerim2429@gmail.com>
2021-09-11 21:10:44 +0900

    chapter6_3

--- Alogrithm/chapter6_3.py
+++ Alogrithm/chapter6_3.py
@@ -1,5 +1,5 @@
 def DFS(v):
-    if v==n+1:
+    if v==n+1: #종료지점 v==4
         for i in range(1,n+1):
             if ch[i]==1:
                 print(i,end='')
@@ -11,6 +11,6 @@ def DFS(v):
         DFS(v+1)
 
 if __name__=="__main__":
-    n=int(input())
+    n=int(input()) #3
     ch=[0]*(n+1)
     DFS(1)
