175fc609934167971f88bfbb33fc1622f4ef92e2
Eden <haerim2429@gmail.com>
2021-09-14 20:32:45 +0900

    1874

--- Alogrithm/BAEKJOON/1874.py
+++ Alogrithm/BAEKJOON/1874.py
@@ -1,22 +1,25 @@
-n = int(input()) 
-s = []
-op = []
-count = 1
-temp = True
-for i in range(n): 
-    num = int(input()) 
-    while count <= num: 
-        s.append(count) 
-        op.append('+') 
-        count += 1
-    if s[-1] == num:
-        s.pop()
-        op.append('-')
-    else:
-        temp = False
+n=int(input())
+s=[]
+op=[]
+count=1
+temp=True
 
-if temp == False:
-    print('NO')
+for i in range(n):
+    num=int(input())
+    while count<=num:
+        s.append(count)
+        op.append("+")
+        count+=1
+
+    if s[-1]==num:
+        s.pop()
+        op.append("-")
+    else:
+        temp=False
+
+
+if temp==False:
+    print("NO")
 else:
     for i in op:
         print(i)
