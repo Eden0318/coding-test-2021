8c1e4156554631e745f04dbdf3ddce7c77a08bd8
Eden <haerim2429@gmail.com>
2021-09-20 20:25:22 +0900

    chapter6_4

--- Alogrithm/chapter6_4.py
+++ Alogrithm/chapter6_4.py
@@ -3,8 +3,8 @@ import sys
 def DFS(L,sum):
     if sum>total//2:
         return
-    if L==n:
-        if sum==(total-sum):
+    if L==n: #L==6
+        if sum==(total-sum): #total-sum은 내가 만들지 않은 반대쪽 부분집합
             print("YES")
             sys.exit(0)
     else:
@@ -12,8 +12,8 @@ def DFS(L,sum):
         DFS(L+1,sum)
 
 if __name__=="__main__":
-    n=int(input())
+    n=int(input())#6
     a=list(map(int,input().split()))
-    total=sum(a)
+    total=sum(a) #32
     DFS(0,0)
     print("NO")
