1edab9bf32e45930cb0dec5e9fb827ba2fc27829
Eden <haerim2429@gmail.com>
2021-09-23 22:54:33 +0900

    17298

---
+++ Alogrithm/BAEKJOON/17298.py
@@ -0,0 +1,14 @@
+import sys
+n = int(input())
+A = list(map(int, sys.stdin.readline().split())) # 3 5 2 7
+answer = [-1] * n #[-1,-1,-1,-1]
+stack = []
+
+stack.append(0)
+for i in range(1, n):
+    while stack and A[stack[-1]] < A[i]:
+        answer[stack.pop()] = A[i]
+    stack.append(i)
+
+print(*answer)
+   
