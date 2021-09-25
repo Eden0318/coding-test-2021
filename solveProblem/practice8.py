81f082b55e3d9ca93918edfac980c3a98b52a25d
Eden <haerim2429@gmail.com>
2021-09-25 20:31:28 +0900

    practice8

--- solveProblem/practice8.py
+++ solveProblem/practice8.py
@@ -1,7 +1,55 @@
-k=[1,2,3,4,5]
-#k.pop(1)
-#print(k)
+import sys
+
+
+def push(X):
+    stack.append(X)
+
+def pop():
+    if stack:
+        return stack.pop()
+    else:
+        return -1
+
+def size():
+    return len(stack)
+
+
+def empty():
+    if stack:
+        return 0
+    else:
+        return 1
+
+def top():
+    if stack:
+        return stack[-1] if stack else -1
+        
+
+
 stack=[]
-if k[2]==3:
-    stack.append(k[2])
-print(stack)
+n=int(input())
+
+for _ in range(n):
+    str=sys.stdin.readline().strip()
+    order=str[0]
+    
+    if order=="push":
+        push(str[1])
+
+    elif order=="pop":
+        print(pop())
+
+    elif order=="size":
+        print(size())
+
+    elif order=="empty":
+        print(empty())
+        
+    elif order=="top":
+        print(top())
+
+
+
+
+
+
