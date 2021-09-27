e49fc3afbfb7548a6b21b07fc0fddd89dfbfa783
Eden <haerim2429@gmail.com>
2021-09-27 12:54:43 +0900

    chapter6_5

--- Alogrithm/chapter6_5.py
+++ Alogrithm/chapter6_5.py
@@ -1,16 +1,18 @@
 import sys
 from collections import deque
 
-def DFS(L,sum):
+def DFS(L,sum,tsum):
     global result
+    if sum+(total-tsum)<result:
+        return
     if sum>c:
         return
     if L==n:
         if sum>result:
             result=sum
     else:
-        DFS(L+1,sum+a[L])
-        DFS(L+1,sum)
+        DFS(L+1,sum+a[L],tsum+a[L])
+        DFS(L+1,sum,tsum+a[L])
 
 if __name__=="__main__":
     c,n=map(int,input().split())
@@ -18,6 +20,7 @@ if __name__=="__main__":
     result=-2147000000
     for i in range(n):
         a[i]=int(input())
-    DFS(0,0)
+    total=sum(a)    
+    DFS(0,0,0)
     print(result)
 
