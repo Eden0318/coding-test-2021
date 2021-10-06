c6f011d8cd9c4ad676c998b075f665e2c8f4d490
Eden <haerim2429@gmail.com>
2021-10-06 20:26:09 +0900

    chapter6_9

--- Alogrithm/chapter6_9.py
+++ Alogrithm/chapter6_9.py
@@ -1,25 +1,18 @@
 #조합문제
-import sys
-def DFS(L):
+def DFS(L,s):
     global cnt
-    if L==m:#m==2
+    if L==m:
         for j in range(L):
-            print(res[j], end=" ")
-        print()
+            print(res[j],end=" ")
         cnt+=1
+        print()
     else:
-        for i in range(1,n+1):
-            if ch[i]==0:
-                ch[i]=1
-                res[L]=i
-                DFS(L+1)
-                ch[i]=0
-
+        for i in range(s,n+1): #4까지 출력
+            res[L]=i
+            DFS(L+1,i+1)
 
 if __name__=="__main__":
-    n,m=map(int,input().split()) # n=4, m=2
-    res=[0]*m
-    ch=[0]*(n+1)
+    n,m=map(int,input().split())
+    res=[0]*(n+1)
     cnt=0
-    DFS(0)
-    print(cnt)
+    DFS(0,1)
