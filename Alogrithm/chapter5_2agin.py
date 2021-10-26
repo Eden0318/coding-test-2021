ee22538fad6a19a048ef2f5fa2edebb363bbf0e7
Eden <haerim2429@gmail.com>
2021-10-26 13:56:07 +0900

    chapter5_2again

---
+++ Alogrithm/chapter5_2agin.py
@@ -0,0 +1,20 @@
+s=input() 
+cnt=0
+stack=[]
+
+for i in range(len(s)): #18
+    if s[i]=="(":
+        s.append(s[i])
+    else:
+        if s[i-1]=="(":
+            stack.pop()
+            cnt+=len(stack)
+        else:
+            stack.pop()
+            cnt+=1    
+        
+print(cnt)
+
+
+
+
