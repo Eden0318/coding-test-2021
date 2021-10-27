349532dd05ec184ace073179d781b1aa6b30a71b
Eden <haerim2429@gmail.com>
2021-10-27 19:27:00 +0900

    chapter5_5again

--- Alogrithm/chapter5_5again.py
+++ Alogrithm/chapter5_5again.py
@@ -5,6 +5,13 @@ dq=list(range(1,n+1))
 dq=deque(dq) 
 
 while dq:
+    for _ in range(k-1):  #0,1 두번 반복
+        cur=dq.popleft() 
+        dq.append(cur) 
+    dq.popleft() 
+    if len(dq)==1:
+        print(dq[0])
+        dq.popleft()
 
 
 
