e24c10a12a6ff7129d83b5baed012245db4fd5d1
Eden <haerim2429@gmail.com>
2021-11-08 20:40:01 +0900

    chapter7_10

--- Alogrithm/chapter7_10.py
+++ Alogrithm/chapter7_10.py
@@ -9,7 +9,10 @@ def DFS(x,y):
         for i in range(4):
             xx=x+dx[i]
             yy=y+dy[i]
-
+            if 0<=xx<=6 and 0<=yy<=6 and board[xx][yy]==0:
+                board[xx][yy]=1
+                DFS(xx,yy)
+                board[xx][yy]=0
 
 
 if __name__=="__main__":
